bd6a64ff8c4d5a05c4bca489704d75bf7d3c9b81
Connor Ring <ccring13@gmail.com>
2016-04-03 20:17:08 -0700

    can scrape first 20 results

--- scraper.py
+++ scraper.py
@@ -1,10 +1,36 @@
 import requests
 from bs4 import BeautifulSoup
 
-request = requests.get("Https://www.google.com/search?q=cute+cats")
+search = raw_input("input a google seach term\n")
 
-content = request.content
+formatted_search = search.replace(" ", "+")
 
-parsed_content = BeautifulSoup(content, "lxml")
+page_one = requests.get("https://www.google.com/search?q=%s" % formatted_search)
+page_two = requests.get("https://www.google.com/search?q=%s&start=10" % formatted_search)
 
-print parsed_content.prettify()
+def get_link_headers(page):
+    page_content = page.content
+    parsed_content = BeautifulSoup(page_content, "html.parser")
+    link_headers = parsed_content.find_all("h3", class_="r")
+    return link_headers
+
+link_headers_one = get_link_headers(page_one)
+link_headers_two = get_link_headers(page_two)
+
+del link_headers_one[0]
+
+print "Here are the first twenty results of your search:\n"
+
+for header in link_headers_one:
+    result_link = header.a["href"][7:]
+    link_content = requests.get(result_link).content
+    parsed_link = BeautifulSoup(link_content, "html.parser")
+    print result_link + "\n"
+    print "Here is some relevant information from the above result:\n"
+    sub_links = parsed_link.find_all("a")
+    for anchor in sub_links:
+        print "   " + anchor["href"]
+
+# for header in link_headers_two:
+#     result_link = header.a["href"][7:]
+#     print result_link + "\n"
